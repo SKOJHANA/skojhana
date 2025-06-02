730f0641acdbc057c3f5efe9e8a27d480d603c7d
SKOJHANA <skojhana@gmail.com>
2025-06-02 03:06:38 +0000

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/*============== toggle icon navbar ==================*/
+/*============== Toggle icon navbar ==================*/
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
@@ -7,35 +7,36 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-/*============== srcoll sections active links ==================*/
+/*============== Scroll sections active links ==================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    let top = window.scrollY;
+
     sections.forEach(sec => {
-        let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*="' + id + '"]').classList.add('active');
             });
-        };
+        }
     });
-/*============== sticky navbar ==================*/
-let header =  document.querySelector('header');
 
-header.classList.toggle('sticky', window.scrollY > 100);
+    /*============== Sticky navbar ==================*/
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
 
-/*============== remove toggle icon and navbar when click navbar link (scroll) ==================*/
+    /*============== Remove toggle icon and navbar when click navbar link ==================*/
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 };
 
-/*============== scroll reveal ==================*/
+/*============== Scroll Reveal ==================*/
 ScrollReveal({
     reset: true,
     distance: '80px',
@@ -44,11 +45,11 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, contact form', {origin: 'bottom'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'});
 ScrollReveal().reveal('.home-content h1, .about-img', {origin: 'left'});
 ScrollReveal().reveal('.home-content p, .about-content', {origin: 'right'});
 
-/*============== typed js ==================*/
+/*============== Typed.js ==================*/
 const typed = new Typed('.multiple-text', {
     strings: ['Entrepreneur', 'Brand Strategist', 'IT Specialist'],
     typeSpeed: 100,
